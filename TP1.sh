4855b1cd9003cc95586c31840a29d07b3d9e49ed
Timur <fracmal17@gmail.com>
2024-04-26 22:27:35 +0300

    Edited 1st HW

--- TP1.sh
+++ TP1.sh
@@ -5,11 +5,7 @@ whoami
 input_dir=$1
 output_dir=$2
 array=()
-for var in $(find $input_dir -type f)
-do
-array+=($var)
-done
-for elem in "${array[@]}"
+for elem in $(find $input_dir -type f)
 do
 sudo cp $elem "$output_dir$(date +%Y%m%d%H%M%S%s%N)_$(basename "$elem")"
 done
